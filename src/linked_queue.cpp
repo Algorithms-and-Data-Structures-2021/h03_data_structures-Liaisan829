180b4abecd394e09debc0b224db49b2d32e3e10f
Liaisan Akhmetova <akhmetova1423@gmail.com>
2021-03-31 22:17:37 +0300

    Done

--- src/linked_queue.cpp
+++ src/linked_queue.cpp
@@ -4,53 +4,68 @@
 
 namespace itis {
 
-void LinkedQueue::Enqueue(Element e) {
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedQueue::Enqueue(Element e) {
 
-void LinkedQueue::Dequeue() {
-  if (size_ == 0) {
-    throw std::logic_error("could not dequeue from empty queue");
-  }
+        auto node = new SinglyNode(e, nullptr);
+        if (size_ == 0) {
+            front_ = node;
+            back_ = node;
+        } else {
+            back_->next = node;
+            back_ = node;
+        }
+        size_++;
+    }
 
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedQueue::Dequeue() {
+        if (size_ == 0) {
+            throw std::logic_error("could not dequeue from empty queue");
+        }
+        auto delete_node = front_;
+        front_ = front_->next;
+        if(!front_) back_ = nullptr;
+        size_--;
+        delete delete_node;
 
-void LinkedQueue::Clear() {
-  // TODO: напишите здесь свой код ...
-}
+    }
+
+    void LinkedQueue::Clear() {
+        // TODO: напишите здесь свой код ...
+        while (size_>0) Dequeue();
+    }
 
 // === РЕАЛИЗОВАНО ===
 
-LinkedQueue::~LinkedQueue() {
-  Clear();
-}
+    LinkedQueue::~LinkedQueue() {
+        Clear();
+    }
 
-std::optional<Element> LinkedQueue::front() const {
-  return front_ == nullptr ? std::nullopt : std::make_optional(front_->data);
-}
+    std::optional<Element> LinkedQueue::front() const {
+        return front_ == nullptr ? std::nullopt : std::make_optional(front_->data);
+    }
 
-std::optional<Element> LinkedQueue::back() const {
-  return back_ == nullptr ? std::nullopt : std::make_optional(back_->data);
-}
+    std::optional<Element> LinkedQueue::back() const {
+        return back_ == nullptr ? std::nullopt : std::make_optional(back_->data);
+    }
 
-bool LinkedQueue::IsEmpty() const {
-  return size_ == 0;
-}
-int LinkedQueue::size() const {
-  return size_;
-}
+    bool LinkedQueue::IsEmpty() const {
+        return size_ == 0;
+    }
+
+    int LinkedQueue::size() const {
+        return size_;
+    }
 
 // === НЕОБХОДИМО ДЛЯ ТЕСТИРОВАНИЯ ===
 
-std::ostream &operator<<(std::ostream &os, const LinkedQueue &queue) {
-  os << "size: " << queue.size_ << '\n';
-  for (auto current_node = queue.front_; current_node != nullptr; current_node = current_node->next) {
-    if (current_node == queue.front_) os << "[FRONT] ";
-    if (current_node == queue.back_) os << "[BACK] ";
-    os << enum2str(current_node->data) << '\n';
-  }
-  return os;
-}
+    std::ostream &operator<<(std::ostream &os, const LinkedQueue &queue) {
+        os << "size: " << queue.size_ << '\n';
+        for (auto current_node = queue.front_; current_node != nullptr; current_node = current_node->next) {
+            if (current_node == queue.front_) os << "[FRONT] ";
+            if (current_node == queue.back_) os << "[BACK] ";
+            os << enum2str(current_node->data) << '\n';
+        }
+        return os;
+    }
 
 }  // namespace itis
